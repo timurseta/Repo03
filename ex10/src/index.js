a87620fdd08262d237fe4562c5abc0bce757a0d8
timurseta <timur_seta@hotmail.com>
2021-09-15 19:37:27 +0200

    product of two variables

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main(numOne, numTwo) {
+
+    // Only change code below this line
+    var numOne;
+    var numTwo;
+    var product;
+
+    product = numOne * numTwo;
+
+    // Only change code above this line
+    return product;
+
+}
+
+console.log(main(425, 10));
+module.exports = main;
