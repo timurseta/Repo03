2469ac72b95280012e34eba5f9e07d7a3c318844
timurseta <timur_seta@hotmail.com>
2021-09-15 15:20:05 +0200

    added index.js with comment

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main () {
+
+    // Only change code below this line
+    // Ovo je prvi komentar
+
+    /* Ovo je drugi komentar iz zadatka */
+
+    //Only change code above this line
+
+}
+
+console.log(main());
+
+module.exports = main;
