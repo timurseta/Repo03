e530f09b9aa7baf7de50849380f73297e5b0ff26
timurseta <timur_seta@hotmail.com>
2021-09-15 19:17:15 +0200

    sum of two variables

---
+++ ex08/src/index.js
@@ -0,0 +1,18 @@
+function main(numOne, numTwo) {
+
+    // Only change code below this line
+
+    var numOne;
+    var numTwo;
+    var sum;
+
+    sum = numOne + numTwo
+
+    // Only change code above this line
+    return sum;
+
+}
+
+console.log(main(150, 425));
+
+module.exports = main;
