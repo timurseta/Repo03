fd3883fa11d2ca00080068a36d71a53f1918e7f9
timurseta <timur_seta@hotmail.com>
2021-09-15 19:25:05 +0200

    difference of two variables

---
+++ ex09/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo) {
+
+    // Only change code below this line
+    var numOne;
+    var numTwo;
+    var difference;
+
+    difference = numOne - numTwo;
+
+    // Only change code above this line
+    return difference;
+
+}
+
+console.log(main(425, 120));
+
+module.exports = main;
